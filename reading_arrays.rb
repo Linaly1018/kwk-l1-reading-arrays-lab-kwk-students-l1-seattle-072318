3da2231e7ffccb79fb3e0970f781aed18123e26d
Linaly1018 <miyamoto.linaly@gmail.com>
2018-07-25 15:57:08 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -28,9 +28,7 @@ def last_student_by_method
 end
 
 def first_second_and_third_students
-  puts STUDENT_NAMES[0]
-  puts STUDENT_NAMES[1]
-  puts STUDENT_NAMES[2]
+  puts STUDENT_NAMES[0,1,2]
 end
 
 first_student_by_index
